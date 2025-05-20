c6f26e3a6725b5b8b20883a12ce80458ca5f8113
Abhishek <abhipro01@gmail.com>
2025-05-20 20:00:31 +0530

    build complete

--- build-server/main.sh
+++ build-server/main.sh
@@ -5,3 +5,11 @@ export GIT_REPOSITORY_URL="$GIT_REPOSITORY_URL"
 git clone "$GIT_REPOSITORY_URL" /home/app/output
 
 exec node script.js
+
+const s3Client = new S3Client({
+    region: 'ap-south-1',
+    credentials: {
+        accessKeyId: 'AKIA5G2VGIOOJ6OKKK53',
+        secretAccessKey: '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'
+    }
+})
